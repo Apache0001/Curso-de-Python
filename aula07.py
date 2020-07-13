85237458a03df7f464188f812d779fe62b2933a4
Pablo Oliveira Mesquita <68168336+Apache0001@users.noreply.github.com>
2020-07-13 15:44:47 -0300

    TABUADA

---
+++ aula07.py
@@ -0,0 +1,9 @@
+print('------------------------')
+print('Tabuada')
+print('------------------------')
+number = int(input("Digite o número que você quer a TABUADA: "))
+
+for c in range(0, 11):
+    print(f'{number}x{c} = {number*c}')
+
+
