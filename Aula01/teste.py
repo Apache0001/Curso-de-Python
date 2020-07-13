a5079179f8edd7373dcf49eae66bc95b8ecc80a8
Pablo Oliveira Mesquita <68168336+Apache0001@users.noreply.github.com>
2020-07-13 18:45:38 -0300

    erro

---
+++ Aula01/teste.py
@@ -0,0 +1,17 @@
+import random
+lista = []
+listord = []
+for i in range(0, 10):
+    lista.append(random.randint(0, 11))
+
+print(lista)
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if lista[i] <= lista[j]:
+            menor = lista[i]
+        
+    listord.append(menor)
+
+print('----------')
+print(listord)
