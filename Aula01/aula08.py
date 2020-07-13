f522efdcf987966efbdf49c53c7e0d1e4615e19a
Pablo Oliveira Mesquita <68168336+Apache0001@users.noreply.github.com>
2020-07-13 16:28:35 -0300

    treinando python

---
+++ Aula01/aula08.py
@@ -0,0 +1,32 @@
+from math import sqrt, ceil
+from random import randint
+num  = int(input("Digite um número: "))
+raiz  = sqrt(num)
+print(ceil(raiz))
+
+vetor = []
+for c in range(0, 10):
+    num2 = randint(0, 11)
+    vetor.append(num2)
+print(vetor)
+maior = vetor[0]
+menor =  vetor[0]
+pmaior = 0
+pmenor = 0
+posMaior = []
+posMenor = []
+
+for i in range(0, 10):
+    if vetor[i] > maior:
+        maior = vetor[i]
+    if vetor[i] < menor:
+        menor = vetor[i]
+
+for i in range(0, 10):
+    if maior == vetor[i]:
+        posMaior.append(i)
+    if menor == vetor[i]:
+        posMenor.append(i)
+
+print(f'O maior valor é: {maior} na posicão: {posMaior}')
+print(f'O menor valor é: {menor} na posição: {posMenor}')
