5f1c75413ffce09c432699c508865396280d2af1
Pablo Oliveira Mesquita <68168336+Apache0001@users.noreply.github.com>
2020-07-13 15:52:41 -0300

    Operacoes aritméticas

---
+++ aula03.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+print(f'A soma vale: {n1+n2}')
+print(f'O produto vale: {n1+n2}')
+print(f'A divisão vale: {n1/n2}')
+print(f'O resto da divisão vale: {n1//n2}')
+print(f'A exponeciação vale: {n1**n2}')
